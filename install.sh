2535eb4b8e1266c3160cff8dbd09f7ce70a139ed
Th3Fanbus <Th3Fanbus@users.noreply.github.com>
2018-06-25 19:54:40 +0200

    Update install.sh
    Add GPG key

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+sudo true # Asks for sudo password before running sudo commands in the background
 git clone https://aur.archlinux.org/aurman.git &
 sudo pacman -S --noconfirm --needed expac git python python-regex python-requests &
+gpg --recv-keys 465022E743D71E39 &
 wait $(jobs -p)
 cd aurman && makepkg -si --noconfirm && cd .. && rm -rf aurman

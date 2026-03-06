5548b74a58f4c531482ef06b775542b1d138813c
Angel Pons <22937129+Th3Fanbus@users.noreply.github.com>
2026-03-06 22:47:14 +0000

    Improve script a bit
    Still, this is polishing a turd.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-sudo true # Asks for sudo password before running sudo commands in the background
+# Install git before using, assume sudo is installed
+sudo pacman -S --noconfirm --needed git
+# Clone and install other deps at the same time
 git clone https://aur.archlinux.org/pakku.git &
-sudo pacman -S --noconfirm --needed --asdeps git curl nim &
+sudo pacman -S --noconfirm --needed base-devel curl nim &
 wait $(jobs -p)
 cd pakku && makepkg -si --noconfirm && cd .. && rm -rf pakku
